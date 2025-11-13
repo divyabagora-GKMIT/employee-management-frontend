d69909cfc12a83fed237be289e21709825e1f2ae
divyabagora-GKMIT <divyabagora@gkmit.co>
2025-11-13 12:47:26 +0530

    chore: update .env.example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import DummyComponent from './components/DummyComponent'
 
 const App = () => {
   return (
     <div>
-        <DummyComponent message="This is the testing message from dummy component"/>
+        Hello
     </div>
   )
 }
