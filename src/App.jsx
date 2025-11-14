db94d37f2974cff5bbc4e0027cc45e4730050f09
divyabagora-GKMIT <divyabagora@gkmit.co>
2025-11-14 13:18:25 +0530

    feature: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>
-        Hello
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
