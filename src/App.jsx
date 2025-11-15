533925b0abc579ae6f0bacfb2782f906bcf7e5e2
divyabagora-GKMIT <divyabagora@gkmit.co>
2025-11-15 22:25:57 +0530

    feature: add reset password page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
+           <Route path="/resetPassword" element={<ResetPassword/>}/>
         </Routes>
       </BrowserRouter>
     </>
