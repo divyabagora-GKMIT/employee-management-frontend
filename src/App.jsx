b114f5e4aeec3a2bfc0c7f3cbecb7cd33c5d8527
divyabagora-GKMIT <divyabagora@gkmit.co>
2025-11-16 12:09:58 +0530

    fix: add private and public wrapper and setup prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import ResetPassword from './pages/ResetPassword'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login/>}/>
-           <Route path="/resetPassword" element={<ResetPassword/>}/>
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/resetPassword"
+            element={
+              <PrivateRoute>
+                <ResetPassword />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
